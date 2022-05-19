4aba5308223530c0c09d85c66455c57e90e385d9
Richard Kirby <richard@getbutlr.com>
2022-05-19 14:03:32 +0100

    extending core module to support additional API features

--- RyftCoreTests/Api/AttemptPaymentRequestTest.swift
+++ RyftCoreTests/Api/AttemptPaymentRequestTest.swift
@@ -20,9 +20,24 @@ class AttemptPaymentRequestTest: XCTestCase {
         )
         XCTAssertTrue(value.clientSecret == "secret")
         XCTAssertTrue(value.cardDetails == expectedCardDetails)
+        XCTAssertNil(value.walletDetails)
     }
 
-    func test_toJson_shouldReturnExpectedValue() {
+    func test_fromApplePay_shouldReturnExpectedValue() {
+        let value = AttemptPaymentRequest.fromApplePay(
+            clientSecret: "secret",
+            applePayToken: "base64-encoded-apple-pay-token"
+        )
+        let expectedWalletDetails = AttemptPaymentRequest.PaymentRequestWalletDetails(
+            type: "ApplePay",
+            applePayToken: "base64-encoded-apple-pay-token"
+        )
+        XCTAssertTrue(value.clientSecret == "secret")
+        XCTAssertTrue(value.walletDetails == expectedWalletDetails)
+        XCTAssertNil(value.cardDetails)
+    }
+
+    func test_toJson_shouldReturnExpectedValue_forCardPayment() {
         let result = AttemptPaymentRequest.fromCard(
             clientSecret: "secret",
             number: "4242424242424242",
@@ -32,6 +47,7 @@ class AttemptPaymentRequestTest: XCTestCase {
         ).toJson()
         XCTAssertNotNil(result["clientSecret"])
         XCTAssertNotNil(result["cardDetails"])
+        XCTAssertNil(result["walletDetails"])
         guard let clientSecret = result["clientSecret"] as? String else {
             XCTFail("serialized JSON clientSecret field was not expected type")
             return
@@ -54,5 +70,33 @@ class AttemptPaymentRequestTest: XCTestCase {
         XCTAssertEqual("10", cardExpiryMonth)
         XCTAssertEqual("2028", cardExpiryYear)
         XCTAssertEqual("100", cardCvc)
+    }
+
+    func test_toJson_shouldReturnExpectedValue_forApplePayPayment() {
+        let result = AttemptPaymentRequest.fromApplePay(
+            clientSecret: "secret",
+            applePayToken: "base64-encoded-apple-pay-token"
+        ).toJson()
+        XCTAssertNotNil(result["clientSecret"])
+        XCTAssertNotNil(result["walletDetails"])
+        XCTAssertNil(result["cardDetails"])
+        guard let clientSecret = result["clientSecret"] as? String else {
+            XCTFail("serialized JSON clientSecret field was not expected type")
+            return
+        }
+        guard let walletDetails = result["walletDetails"] as? [String: Any] else {
+            XCTFail("serialized JSON walletDetails field was not expected type")
+            return
+        }
+        guard
+            let walletType = walletDetails["type"] as? String,
+            let applePayToken = walletDetails["applePayToken"] as? String
+        else {
+            XCTFail("serialized JSON walletDetails did not contain the expected fields")
+            return
+        }
+        XCTAssertEqual("secret", clientSecret)
+        XCTAssertEqual("ApplePay", walletType)
+        XCTAssertEqual("base64-encoded-apple-pay-token", applePayToken)
     }
 }
