9ea83964f748bfb9ebbb7f79863ecf099e52607f
ryft-rkirby <78797543+ryft-rkirby@users.noreply.github.com>
2022-06-30 15:53:11 +0100

    Update User-Agent to latest release version

--- RyftCore/Source/Constants.swift
+++ RyftCore/Source/Constants.swift
@@ -1,6 +1,6 @@
 struct Constants {
 
-    static let version = "1.0.0"
+    static let version = "1.1.0"
     static let productName = "ryft-ios"
     static let userAgent = "ryft-sdk-ios/\(version)"
 }
