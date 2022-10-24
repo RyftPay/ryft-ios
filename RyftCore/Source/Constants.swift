31b2efdf8133285cd44b19f96c616316e97eecda
Richard Kirby <richard@getbutlr.com>
2022-10-24 14:53:50 +0100

    bump version to 1.3.0

--- RyftCore/Source/Constants.swift
+++ RyftCore/Source/Constants.swift
@@ -1,6 +1,6 @@
 struct Constants {
 
-    static let version = "1.2.0"
+    static let version = "1.3.0"
     static let productName = "ryft-ios"
     static let userAgent = "ryft-sdk-ios/\(version)"
 }
