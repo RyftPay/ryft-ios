608d91878cac1d49a19a22c4a1e7d4ba2492d0a1
Richard Kirby <richard@getbutlr.com>
2024-10-25 10:10:27 +0100

    increment version to 1.6.1

--- RyftCore/Source/Constants.swift
+++ RyftCore/Source/Constants.swift
@@ -1,6 +1,6 @@
 struct Constants {
 
-    static let version = "1.6.0"
+    static let version = "1.6.1"
     static let productName = "ryft-ios"
     static let userAgent = "ryft-sdk-ios/\(version)"
 }
