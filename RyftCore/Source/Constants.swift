cf18be81b32a71fa1b787944d649ba94de5afa82
Rob Bradbury <robbradbury@ryftpay.com>
2026-08-10 09:05:09 +0100

    Rp 1876 sandbox directory server (#23)
    * [RP-1876] Fix sandbox directory server ID
    
    * [RP-1876] Version bump

--- RyftCore/Source/Constants.swift
+++ RyftCore/Source/Constants.swift
@@ -1,6 +1,6 @@
 struct Constants {
 
-    static let version = "2.0.0"
+    static let version = "2.0.1"
     static let productName = "ryft-ios"
     static let userAgent = "ryft-sdk-ios/\(version)"
 }
